08cb0e898491d979d4c24308ead573cfb71c5d3e
Srirang <srirangr@gmail.com>
2017-08-06 12:20:01 +0530

    Yearbook added

---
+++ nav.js
@@ -0,0 +1,13 @@
+var navElements = {
+	"About" : "index.html",
+	"Work" : "work.html",
+	"Contact" : "index.html#contact",
+	"Views" : "views.html"
+}
+
+function buildNav(obj, root) {
+	
+}
+
+var navRoot = $('#navbar');
+buildNav(navElements, navRoot);
